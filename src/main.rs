8fb19011a9b8006fd22e749f5a4db06b1987c262
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-12-03 23:45:43 +0000

    feat: make server more modular

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use std::net::TcpListener;
+
 use zero_to_prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    match run() {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let rand_port = listener.local_addr().unwrap().port();
+
+    match run(listener) {
         Ok(srv) => {
-            println!("Server listeneing on 8000");
+            println!("Server listeneing on {}", rand_port);
             srv.await
         }
         Err(e) => Err(e)?,
