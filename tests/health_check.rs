1e8096e38f65b45552f0425082af421a69f06b55
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-12-07 20:49:26 +0000

    chore: bump sqlx version

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -27,7 +27,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 }
 
-#[tokio::test] 
+#[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     let address = spawn_app();
     let client = reqwest::Client::new();
@@ -35,7 +35,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("", "missing both name and email")
+        ("", "missing both name and email"),
     ];
 
     for (invalid_body, error_msg) in test_cases {
