87d2e74486f6c7ea29f090a76f05b9b228917a6e
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-12-17 23:13:13 +0000

    feat: add tracing for endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use zero_to_prod::{configuration::get_configuration, startup::run};
+use zero_to_prod::{
+    configuration::get_configuration, 
+    startup::run, 
+    telementry::{get_subscriber, init_subscriber}
+};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configurations");
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
@@ -12,11 +19,5 @@ async fn main() -> Result<(), std::io::Error> {
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address)?;
 
-    match run(listener, connection) {
-        Ok(srv) => {
-            println!("Server listening on `{}`", address);
-            srv.await
-        }
-        Err(e) => Err(e)?,
-    }
+    run(listener, connection)?.await
 }
