f02b39ed5d31e5ce50aa5540bde0bb2b38dbe54d
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2025-01-02 23:35:10 +0000

    feat: add sslmode in db connection and env in do spec

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions};
 use zero_to_prod::{
     configuration::get_configuration,
     startup::run,
@@ -13,8 +13,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configurations");
-    let connection = PgPool::connect_lazy(&configuration.database.connection_string())
-        .expect("Failed to connect to postgres");
+    let connection = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(&address)?;
 
