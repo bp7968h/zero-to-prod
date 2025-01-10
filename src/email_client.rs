1cf904fcab35f83f2f0d27754be627a182987685
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2025-01-10 00:18:08 +0000

    fix: complete test for the reqwest

--- src/email_client.rs
+++ src/email_client.rs
@@ -30,11 +30,11 @@ impl EmailClient {
     ) -> Result<(), reqwest::Error> {
         let url = self.base_url.clone();
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject,
+            html_body: html_content,
+            text_body: text_content,
         };
         self.http_client.post(&url)
             .basic_auth("api", Some(self.key.expose_secret()))
@@ -46,23 +46,23 @@ impl EmailClient {
     }
 }
 
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    text_body: String,
-    html_body: String,
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    text_body: &'a str,
+    html_body: &'a str,
 }
 
-impl From<SendEmailRequest> for Form {
+impl<'a> From<SendEmailRequest<'a>> for Form {
     fn from(value: SendEmailRequest) -> Self {
         let mut form = Form::new();
         form = form
-            .text("from", value.from)
-            .text("to", value.to)
-            .text("subject", value.subject)
-            .text("text", value.text_body)
-            .text("html", value.html_body);
+            .text("from", value.from.to_owned())
+            .text("to", value.to.to_owned())
+            .text("subject", value.subject.to_owned())
+            .text("text", value.text_body.to_owned())
+            .text("html", value.html_body.to_owned());
 
         form
     }
@@ -76,17 +76,21 @@ mod tests {
     use fake::faker::internet::en::SafeEmail;
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
-    use wiremock::matchers::any;
+    use wiremock::matchers::{basic_auth, method};
     use wiremock::{Mock, MockServer, ResponseTemplate};
     use secrecy::SecretBox;
+    use wiremock_multipart::prelude::*;
     
     #[tokio::test]
-    async fn send_email_fires_a_request_to_base_url() {
+    async fn send_email_sends_the_expected_request() {
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let email_client = EmailClient::new(mock_server.uri(), sender, SecretBox::new(Faker.fake()));
+        let api_key: Box<String> = Box::new(Faker.fake());
+        let email_client = EmailClient::new(mock_server.uri(), sender, SecretBox::new(api_key.clone()));
 
-        Mock::given(any())
+        Mock::given(basic_auth("api", *api_key))
+            .and(method("POST"))
+            .and(NumberOfParts(5))
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
